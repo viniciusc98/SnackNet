15b7257a34351cf95740780a44b4d14c6c7ec41a
lopeswestphal <lopeswestphal@hotmail.com>
2020-04-08 18:57:31 -0300

    feat: Adicionei e modifiquei os metodos da classe vendedor

--- src/snacknet/Vendedor.java
+++ src/snacknet/Vendedor.java
@@ -1,54 +1,75 @@
 package snacknet;
 
+import java.util.Objects;
+
 public class Vendedor {
     private String nomeVendedor;
     private String campus;
     private Integer estoque;
 
-    public Vendedor registrarVendedor(String nomeVendedor, String campus, Integer estoque)throws Exception{
-        if(campus.isEmpty()){
-            throw new Exception("campus esta vazio"); 
-        }
-
-        if(nomeVendedor.isEmpty()){
-            throw new Exception("Nome vazio");
-        }
-
-        if(estoque == 0){
-            throw new Exception("Estoque vazio");
-        }
-
-        Vendedor vendedor01 = new Vendedor();
-
-        vendedor01.setNomeVendedor("pamonha");
-        vendedor01.setCampus(campus);
-        vendedor01.setEstoque(10);
-
-        return vendedor01;
+    // Metodo construtor: 
+    public Vendedor(String nomeVendedor, String campus, Integer estoque) {
+        this.nomeVendedor = nomeVendedor;
+        this.campus = campus;
+        this.estoque = estoque;
+    }
+    
+    // Metodo hashCode e equals:
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 41 * hash + Objects.hashCode(this.estoque);
+        return hash;
     }
 
-    public String getNomeVendedor(){
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Vendedor other = (Vendedor) obj;
+        if (!Objects.equals(this.estoque, other.estoque)) {
+            return false;
+        }
+        return true;
+    }
+    
+    // Metodo toString:
+    @Override
+    public String toString() {
+        return "Vendedor{" + "nomeVendedor=" + nomeVendedor + ", campus=" + campus + '}';
+    }
+    
+    // Metodos Especiais:
+    public String getNomeVendedor() {
         return nomeVendedor;
     }
-
+    
     public void setNomeVendedor(String nomeVendedor) {
         this.nomeVendedor = nomeVendedor;
     }
 
-    public String getCampus(){
+    public String getCampus() {
         return campus;
     }
 
-    public void setCampus(String campus){
+    public void setCampus(String campus) {
         this.campus = campus;
     }
 
-    public Integer getEstoque(){
+    public Integer getEstoque() {
         return estoque;
     }
 
     public void setEstoque(Integer estoque) {
         this.estoque = estoque;
     }
-
+    
+    
 }
